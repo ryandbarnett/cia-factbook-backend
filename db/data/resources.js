34a7cd6a8c546e61508c172254c928df109c0b53
RyanDBarnett <conner2468@gmail.com>
2019-06-27 11:22:09 -0600

    Add raw data files

---
+++ db/data/resources.js
@@ -0,0 +1,626 @@
+module.exports = [
+  {
+    "name": "abundant hydropower"
+  },
+  {
+    "name": "agricultural"
+  },
+  {
+    "name": "aloes"
+  },
+  {
+    "name": "alumina"
+  },
+  {
+    "name": "aluminum"
+  },
+  {
+    "name": "amber"
+  },
+  {
+    "name": "antimony"
+  },
+  {
+    "name": "arable land"
+  },
+  {
+    "name": "aragonite"
+  },
+  {
+    "name": "arsenic"
+  },
+  {
+    "name": "asbestos"
+  },
+  {
+    "name": "asphalt"
+  },
+  {
+    "name": "barite"
+  },
+  {
+    "name": "barites"
+  },
+  {
+    "name": "basalt rock"
+  },
+  {
+    "name": "bauxite"
+  },
+  {
+    "name": "bauxite; unexploited"
+  },
+  {
+    "name": "bismuth"
+  },
+  {
+    "name": "borate"
+  },
+  {
+    "name": "brown coal"
+  },
+  {
+    "name": "cadmium"
+  },
+  {
+    "name": "calcium"
+  },
+  {
+    "name": "calcium carbonate"
+  },
+  {
+    "name": "calcium phosphates"
+  },
+  {
+    "name": "carbonates"
+  },
+  {
+    "name": "cassiterite"
+  },
+  {
+    "name": "celestite"
+  },
+  {
+    "name": "chalk"
+  },
+  {
+    "name": "chicle"
+  },
+  {
+    "name": "chromate"
+  },
+  {
+    "name": "chrome"
+  },
+  {
+    "name": "chrome ore"
+  },
+  {
+    "name": "chromite"
+  },
+  {
+    "name": "chromium"
+  },
+  {
+    "name": "chromium ore"
+  },
+  {
+    "name": "cinnamon trees"
+  },
+  {
+    "name": "citrus"
+  },
+  {
+    "name": "clay"
+  },
+  {
+    "name": "clay earth pigment"
+  },
+  {
+    "name": "clays"
+  },
+  {
+    "name": "climate"
+  },
+  {
+    "name": "coal"
+  },
+  {
+    "name": "cobalt"
+  },
+  {
+    "name": "cocoa beans"
+  },
+  {
+    "name": "coconut"
+  },
+  {
+    "name": "coffee"
+  },
+  {
+    "name": "conch"
+  },
+  {
+    "name": "copper"
+  },
+  {
+    "name": "copper ore"
+  },
+  {
+    "name": "copra"
+  },
+  {
+    "name": "crude oil reserves"
+  },
+  {
+    "name": "deep seabed minerals"
+  },
+  {
+    "name": "deepwater ports"
+  },
+  {
+    "name": "diamond"
+  },
+  {
+    "name": "diatomite"
+  },
+  {
+    "name": "dolomite"
+  },
+  {
+    "name": "dolomitic limestone"
+  },
+  {
+    "name": "emeralds"
+  },
+  {
+    "name": "emery"
+  },
+  {
+    "name": "extensive natural gas reserves"
+  },
+  {
+    "name": "feldspar"
+  },
+  {
+    "name": "ferrosilicon"
+  },
+  {
+    "name": "fish"
+  },
+  {
+    "name": "fisheries"
+  },
+  {
+    "name": "fluorite"
+  },
+  {
+    "name": "fluorspar"
+  },
+  {
+    "name": "forests"
+  },
+  {
+    "name": "gallium"
+  },
+  {
+    "name": "gas"
+  },
+  {
+    "name": "gas deposits"
+  },
+  {
+    "name": "gem diamonds"
+  },
+  {
+    "name": "gems"
+  },
+  {
+    "name": "gemstones"
+  },
+  {
+    "name": "geothermal power"
+  },
+  {
+    "name": "germanium"
+  },
+  {
+    "name": "gold"
+  },
+  {
+    "name": "gold deposits"
+  },
+  {
+    "name": "granite"
+  },
+  {
+    "name": "graphite"
+  },
+  {
+    "name": "gravel"
+  },
+  {
+    "name": "grazing land"
+  },
+  {
+    "name": "gypsum"
+  },
+  {
+    "name": "hafnium"
+  },
+  {
+    "name": "hard coal"
+  },
+  {
+    "name": "hardwood forests"
+  },
+  {
+    "name": "hardwood timber"
+  },
+  {
+    "name": "hardwoods"
+  },
+  {
+    "name": "helium"
+  },
+  {
+    "name": "hydroelectric potential"
+  },
+  {
+    "name": "hydroelectricity"
+  },
+  {
+    "name": "hydropower"
+  },
+  {
+    "name": "ilmenite"
+  },
+  {
+    "name": "indium"
+  },
+  {
+    "name": "industrial diamonds"
+  },
+  {
+    "name": "iron"
+  },
+  {
+    "name": "iron ore"
+  },
+  {
+    "name": "kaolin"
+  },
+  {
+    "name": "lead"
+  },
+  {
+    "name": "lignite"
+  },
+  {
+    "name": "limestone"
+  },
+  {
+    "name": "limited petroleum"
+  },
+  {
+    "name": "liquefied natural gas"
+  },
+  {
+    "name": "lithium"
+  },
+  {
+    "name": "lobster"
+  },
+  {
+    "name": "magnesite"
+  },
+  {
+    "name": "magnesium"
+  },
+  {
+    "name": "magnesium bromide"
+  },
+  {
+    "name": "magnetite"
+  },
+  {
+    "name": "mahogany forests"
+  },
+  {
+    "name": "manganese"
+  },
+  {
+    "name": "manganese deposits"
+  },
+  {
+    "name": "manganese ore"
+  },
+  {
+    "name": "marble"
+  },
+  {
+    "name": "marine products"
+  },
+  {
+    "name": "marl"
+  },
+  {
+    "name": "mercury"
+  },
+  {
+    "name": "methane"
+  },
+  {
+    "name": "mica"
+  },
+  {
+    "name": "mineral sands"
+  },
+  {
+    "name": "mineral springs"
+  },
+  {
+    "name": "mineral water"
+  },
+  {
+    "name": "minerals"
+  },
+  {
+    "name": "micro trees"
+  },
+  {
+    "name": "molybdenum"
+  },
+  {
+    "name": "natron"
+  },
+  {
+    "name": "natural gas"
+  },
+  {
+    "name": "nepheline"
+  },
+  {
+    "name": "nickel"
+  },
+  {
+    "name": "niobium"
+  },
+  {
+    "name": "nitrates"
+  },
+  {
+    "name": "nonferrous metals"
+  },
+  {
+    "name": "offshore oil"
+  },
+  {
+    "name": "oil"
+  },
+  {
+    "name": "oil shale"
+  },
+  {
+    "name": "other minerals"
+  },
+  {
+    "name": "palm oil"
+  },
+  {
+    "name": "peanuts"
+  },
+  {
+    "name": "pearls"
+  },
+  {
+    "name": "peat"
+  },
+  {
+    "name": "peat deposits"
+  },
+  {
+    "name": "perlite"
+  },
+  {
+    "name": "petroleum"
+  },
+  {
+    "name": "phosphate"
+  },
+  {
+    "name": "phosphate rock"
+  },
+  {
+    "name": "phosphates"
+  },
+  {
+    "name": "phosphorite"
+  },
+  {
+    "name": "phosphorites"
+  },
+  {
+    "name": "platinum"
+  },
+  {
+    "name": "platinum group metals"
+  },
+  {
+    "name": "potash"
+  },
+  {
+    "name": "precious metals"
+  },
+  {
+    "name": "precious stones"
+  },
+  {
+    "name": "pumice"
+  },
+  {
+    "name": "pumicite"
+  },
+  {
+    "name": "pyrite"
+  },
+  {
+    "name": "pyrites"
+  },
+  {
+    "name": "quarry stone"
+  },
+  {
+    "name": "quartz"
+  },
+  {
+    "name": "rare earth elements"
+  },
+  {
+    "name": "rare earth metals"
+  },
+  {
+    "name": "rare earth oxides"
+  },
+  {
+    "name": "rare woods"
+  },
+  {
+    "name": "rock salt"
+  },
+  {
+    "name": "rubber"
+  },
+  {
+    "name": "rutile"
+  },
+  {
+    "name": "salt"
+  },
+  {
+    "name": "sand"
+  },
+  {
+    "name": "sea mud"
+  },
+  {
+    "name": "selenium"
+  },
+  {
+    "name": "semiprecious stones"
+  },
+  {
+    "name": "sepiolite"
+  },
+  {
+    "name": "shale oil"
+  },
+  {
+    "name": "shrimp"
+  },
+  {
+    "name": "silica"
+  },
+  {
+    "name": "silica sand"
+  },
+  {
+    "name": "silver"
+  },
+  {
+    "name": "slate"
+  },
+  {
+    "name": "soda ash"
+  },
+  {
+    "name": "soft coal"
+  },
+  {
+    "name": "some petroleum"
+  },
+  {
+    "name": "sorghum"
+  },
+  {
+    "name": "spiny lobster"
+  },
+  {
+    "name": "stone"
+  },
+  {
+    "name": "strontium"
+  },
+  {
+    "name": "sulfur"
+  },
+  {
+    "name": "talc"
+  },
+  {
+    "name": "tantalite"
+  },
+  {
+    "name": "tantalum"
+  },
+  {
+    "name": "tar sands"
+  },
+  {
+    "name": "tea"
+  },
+  {
+    "name": "tellurium"
+  },
+  {
+    "name": "timber"
+  },
+  {
+    "name": "tin"
+  },
+  {
+    "name": "tin ore"
+  },
+  {
+    "name": "titanium"
+  },
+  {
+    "name": "titanium ore"
+  },
+  {
+    "name": "tropical fruit"
+  },
+  {
+    "name": "tungsten"
+  },
+  {
+    "name": "tungsten ore"
+  },
+  {
+    "name": "uranium"
+  },
+  {
+    "name": "vanadium"
+  },
+  {
+    "name": "vegetables"
+  },
+  {
+    "name": "water"
+  },
+  {
+    "name": "whales"
+  },
+  {
+    "name": "wildlife"
+  },
+  {
+    "name": "wolframite"
+  },
+  {
+    "name": "zinc"
+  },
+  {
+    "name": "zircon"
+  }
+];
