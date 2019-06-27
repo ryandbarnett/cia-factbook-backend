6a7c61ce3215075a08fb89179d72e67eba711c42
RyanDBarnett <conner2468@gmail.com>
2019-06-27 13:05:34 -0600

    Create countries get endpoint

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const enviroment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const express = require('express');
@@ -12,12 +12,12 @@ app.listen(port, () => {
   console.log(`App is running on ${port}`)
 });
 
-app.get('api/v1/countries', (request, response) => {
+app.get('/api/v1/countries', (request, response) => {
   database('countries').select()
-  .then((countries) => {
-    response.status(200).json(countries);
-  })
-  .catch(error => {
-    response.status(404).send(error)
-  })
+    .then((countries) => {
+      response.status(200).json(countries);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 })
